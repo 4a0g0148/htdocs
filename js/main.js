60c72cda8f5cfd1f76af5da931320fd058f21610
4a0g0148 <4a0g0148@stust.edu.tw>
2014-08-13 09:34:52 +0800

    v1.0.6

--- js/main.js
+++ js/main.js
@@ -12,11 +12,38 @@
 	
 	
 
-$(document).ready(function(){
+$(document).ready(function(){     
+	
+
   $(".mouseevent").mouseover(function(){
 	  $(this).css("background-color","#81B2CC");
+	  
   });
   $(".mouseevent").mouseout(function(){
 	  $(this).css("background-color","#89B9D2");
+	  
   });
+  
+  
+  
+  
+  $("#menu").mouseenter(function(){
+	  $("#menu").click;
+	  $("#menu2").show(250);  
+	  });
+
+  
+  $("#menu2").mouseleave(function(){
+	  $("#menu2").hide();
+	  });
+  
+  $(".navbar-brand,#li1,#myCarousel,.navbar-collapse collapse,.navbar-header").mouseenter(function(){
+	  $("#menu").css("background-color","#222");
+	  $("#menu2").hide();  
+	  });
+  
 });
+
+
+
+
